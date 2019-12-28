96236cd9d624f6c51406393f6e761e3dfb2f12e7
avinash <avinash.patel@kuliza.com>
2019-12-28 11:36:23 +0530

    To Be Continued

---
+++ src/main/java/com/primaryDI/PrimaryQualifierDiApplication.java
@@ -0,0 +1,13 @@
+package com.primaryDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryQualifierDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaryQualifierDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primaryDI/PrimaryQualifierDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primaryDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryQualifierDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
